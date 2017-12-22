8f353981d6cc105cc114157f7e0ed9c811e32bd9
J-M <jean-michel.perraud@csiro.au>
2017-12-22 16:41:29 +1100

    Up version number.

--- bindings/R/pkgs/cinterop/R/cinterop-package.r
+++ bindings/R/pkgs/cinterop/R/cinterop-package.r
@@ -5,14 +5,15 @@
 #' \tabular{ll}{
 #' Package: \tab cinterop \cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.2-4 \cr
-#' Date: \tab 2017-08-09 \cr
-#' Release Notes: \tab Add unit tests and additional time series conversions for dependent packages \cr
+#' Version: \tab 0.2.5 \cr
+#' Date: \tab 2017-12-22 \cr
+#' Release Notes: \tab Expand features relating to interop of time series includint xts in the R package cinterop \cr
 #' License: \tab LGPL-3 \cr
 #' }
 #'
 #' \tabular{lll}{
 #' Version \tab Date \tab Notes \cr
+#' 0.2.4 \tab 2017-08-09 \tab Add unit tests and additional time series conversions for dependent packages \cr
 #' }
 #'
 #' @name cinterop-package
