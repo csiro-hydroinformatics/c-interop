b83ecd1b29f6c12b0f81c02d2278f1824472d196
J-M <jean-michel.perraud@csiro.au>
2020-06-11 11:39:47 +1000

    R package version update

--- bindings/R/pkgs/cinterop/R/cinterop-package.r
+++ bindings/R/pkgs/cinterop/R/cinterop-package.r
@@ -5,14 +5,15 @@
 #' \tabular{ll}{
 #' Package: \tab cinterop \cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.3.0 \cr
-#' Date: \tab 2019-08-03 \cr
-#' Release Notes: \tab Fixes to the R cinterop package, required by R 3.6+ which has likely broken the `prototype` argument in setClass. Issue #4: https://github.com/csiro-hydroinformatics/rcpp-interop-commons/issues/4 \cr
-#' License: \tab LGPL-3 \cr
+#' Version: \tab 1.0 \cr
+#' Date: \tab 2020-06-10 \cr
+#' Release Notes: \tab Adding C interop code for the definition of multi-objective multi-site optimisation\cr
+#' License: \tab BSD-3 \cr
 #' }
 #'
 #' \tabular{lll}{
 #' Version \tab Date \tab Notes \cr
+#' 0.3.0 \tab 2019-08-03 \tab Fixes to the R cinterop package, required by R 3.6+ which has likely broken the `prototype` argument in setClass. Issue #4: https://github.com/csiro-hydroinformatics/rcpp-interop-commons/issues/4 \cr
 #' 0.2.5 \tab 2017-12-22 \tab Expand features relating to interop of time series includint xts in the R package cinterop \cr
 #' 0.2.4 \tab 2017-08-09 \tab Add unit tests and additional time series conversions for dependent packages \cr
 #' }
